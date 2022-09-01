c9d0d016681e75a28b20513723d99b02dc957a21
HenryPeeples <hgpeeples48@gmail.com>
2022-09-01 15:25:15 -0500

    [utils]: Update prefix and add embed error handling

--- utils/utils.go
+++ utils/utils.go
@@ -4,15 +4,23 @@ package utils
 import (
 	"os"
 
+	"github.com/bwmarrin/discordgo"
 	logger "github.com/withmandala/go-log"
 )
 
 // Prefix is the bot command character prefix
-const Prefix string = "?"
+const Prefix string = "!"
 
 var log = NewLogger()
 
 // NewLogger returns a new instance of a logger
 func NewLogger() *logger.Logger {
 	return logger.New(os.Stdout).WithColor()
+}
+
+// HandleEmbedFailure delivers a message to the channel that something
+// went wrong when trying to send an embed
+func HandleEmbedFailure(s *discordgo.Session, m *discordgo.MessageCreate, err error) {
+	s.ChannelMessageSend(m.ChannelID, "Something broke... Couldn't send embedded message.")
+	log.Error("Embed error: ", err)
 }
